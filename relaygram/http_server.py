19b6584f06ca3ac6736abd82eef4920d8a51f0a8
Vincent Castellano <surye80@gmail.com>
2016-04-28 10:49:22 -0700

    Static file server for Telegram media.

--- relaygram/http_server.py
+++ relaygram/http_server.py
@@ -1,5 +1,41 @@
 import http.server
+from threading import Thread
+import os.path
 
 
-class HTTPHandler(http.server.BaseHTTPRequestHandler):
-    pass
+class HTTPHandler:
+    def __init__(self, verbosity, config):
+        self.verbosity = verbosity
+        self.config = config
+
+        handler = HTTPHandler.make_http_handler('C:/tmp/test/')
+        self.httpd = http.server.HTTPServer(('', 8000), handler)
+
+        self.thread = Thread(target=self.main_loop)
+
+    def run(self):
+        self.thread.start()
+        return self
+
+    def main_loop(self):
+        self.httpd.serve_forever()
+
+    @staticmethod
+    def make_http_handler(root_path):
+        class RelayGramHTTPHandler(http.server.BaseHTTPRequestHandler):
+            def __init__(self, *args, **kwargs):
+                super(RelayGramHTTPHandler, self).__init__(*args, **kwargs)
+
+            def do_GET(self):
+                file_path = os.path.abspath(root_path + self.path)
+
+                if os.path.commonpath([root_path, file_path]) != os.path.abspath(root_path):  # Detect path traversal attempt
+                    self.send_error(501, "Nice try")
+                else:
+                    if not os.path.exists(file_path) or not os.path.isfile(file_path):
+                        self.send_error(404, 'File Not Found')
+                    else:
+                        self.send_response(200)
+                        self.wfile.write(open(file_path, mode='rb').read())
+
+        return RelayGramHTTPHandler
