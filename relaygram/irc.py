23d77a5c0f2e22a026bfc1c9144e8107be834146
Vincent Castellano <surye80@gmail.com>
2016-04-28 23:22:38 -0700

    Don't send blank lines to IRC.

--- relaygram/irc.py
+++ relaygram/irc.py
@@ -96,8 +96,9 @@ class IRCHandler:
         if msg:
             self.log.info("Sending to irc: {msg}".format(msg=msg))
             for line in msg.splitlines():
-                self.irc_servers[server].privmsg(channel, line)
-                sleep(0.75)
+                if line.strip() != '':
+                    self.irc_servers[server].privmsg(channel, line)
+                    sleep(0.75)
 
     def irc_pubmsg(self, connection, event):
         item = events.Message(src=(connection.server, event.target), user=event.source.nick, msg=event.arguments[0])
